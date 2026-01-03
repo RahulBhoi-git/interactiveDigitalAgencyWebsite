e38e81fb9f79886adac0ca235c2851a9f000ccea
Rahul Bhoi <rahulbhoi7894@gmail.com>
2026-01-04 00:22:01 +0530

    Inital commit

---
+++ src/components/Services.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import assets from "../assets/assets";
+import Title from "./Title";
+import ServiceCard from "./ServiceCard";
+import { motion } from "motion/react";
+const Services = () => {
+  const servicesData = [
+    {
+      title: "Advertising",
+      description:
+        "We turn bold ideas into powerful digital solutions that connect ,engage...",
+      icon: assets.ads_icon,
+    },
+    {
+      title: "Content marketing",
+      description: "We help you execute your plan and delicer results.",
+      icon: assets.marketing_icon,
+    },
+    {
+      title: "Content writing",
+      description:
+        "We help you to create a marketing stategy that drives results.",
+      icon: assets.content_icon,
+    },
+    {
+      title: "Social Media",
+      description:
+        "We help you build strong social media presence and engage with your audience.",
+      icon: assets.social_icon,
+    },
+  ];
+  return (
+    <motion.div
+    initial="hidden"
+    whileInView="visible"
+    viewport={{once:true}}
+    transition={{staggerChildren:0.2}}
+    
+      id="services"
+      className="relative flex flex-col items-center
+    gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-grey-700 dark:text-white"
+    >
+      <img
+        src={assets.bgImage2}
+        alt=""
+        className="absolute -top-110 -left-70 -z-1 dark:hidden"
+      />
+      <Title
+        title="How can we help?"
+        desc="From strategy to execution,we craft digital solution that moce your bussiness forward."
+      />
+
+      <div className="flex flex-col md:grid grid-cols-2">
+        {servicesData.map((service,index)=>(
+
+        <ServiceCard key={index} service={service} index={index}/>
+      ))}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Services;
---
+++ src/components/Title.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { motion } from "motion/react";
+const Title = ({ title, desc }) => {
+  return (
+    <div>
+      <motion.h2
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        viewport={{ once: true }}
+        className="text-3xl text-center sm:text-5xl font-medium pb-10"
+      >
+        {title}
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5,delay:0.2 }}
+        viewport={{ once: true }}
+        className="max-w-lg text-center text-grey-500 dark:text-white/75 mb-6"
+      >
+        {desc}
+      </motion.p>
+    </div>
+  );
+};
+
+export default Title;
